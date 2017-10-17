14cc2bbb63e3e1a6e23e48cc22ad64ac790d9398
Leszek Bonikowski <bonikowski.leszek@gmail.com>
2017-10-17 12:58:44 +0200

    initial projet version

---
+++ server.R
@@ -0,0 +1,34 @@
+#+ATTR_LATEX: :options fontsize=\scriptsize 
+#+BEGIN_SRC R :eval no :exports code
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+    })
+}
+#+END_SRC
+** App
+#+BEGIN_SRC R :eval no :exports code
+library(shiny)
+
+# See above for the definitions of ui and server
+ui <- ...
+
+server <- ...
+
+shinyApp(ui = ui, server = server)
+#+END_SRC
+
