a160df9d8f1b11e268b18444697285eb4d831947
TheChoiceOfSteinsGate <kacekg@hotmail.com>
2017-11-14 13:11:35 +0100

    75

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ ui <- fluidPage(
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 1,
-                  max = 60,
+                  max = 75,
                   value = 30)
     ),
     # Main panel for displaying outputs ----
